7658cd65f4a2defae57237115b4e933744675a7c
David Knotek <davks11@gmail.com>
2024-07-16 11:53:07 +0200

    Aoc 2015 - day 1 finished.

--- src/main/kotlin/aoc2023/day4/Day4.kt
+++ src/main/kotlin/aoc2023/day4/Day4.kt
@@ -3,10 +3,11 @@ package aoc2023.day4
 import aoc2023.day3.loadFile
 import kotlin.math.pow
 
-val puzzleInput = loadFile("src/main/kotlin/aoc2023/day4/input.txt")
+val puzzleInput = loadFile("src/main/kotlin/aoc2023/day4/test-input.txt")
 
 fun main() {
     doFirst(puzzleInput)
+    doSecond(puzzleInput)
 }
 
 fun doFirst(puzzleInput: List<String>) {
@@ -21,4 +22,34 @@ fun doFirst(puzzleInput: List<String>) {
         result.add(2.0.pow(foundNumbers.count() - 1).toInt())
     }
     println(result.sumOf { it })
+}
+
+fun doSecond(puzzleInput: List<String>) {
+    val totalCount = mutableMapOf<Int, Int>()
+    puzzleInput.forEachIndexed() { index, row ->
+        val (left, right) = row.split(":")[1].split("|")
+
+        val leftNumbers = "(\\d+)".toRegex().findAll(left).map { it.value }.toList()
+        val rightNumbers = "(\\d+)".toRegex().findAll(right).map { it.value }.toList()
+
+        val foundNumbers = leftNumbers.filter { it in rightNumbers }
+
+//        println(foundNumbers.count())
+
+        totalCount[index + 1] = foundNumbers.count()
+
+    }
+    println(totalCount)
+    val winningsNumberCount = totalCount.toList()
+    println(winningsNumberCount)
+
+    winningsNumberCount.forEach() { (cardNumber, winningsNumberCount) ->
+        repeat(winningsNumberCount) {
+            println("$cardNumber $it $winningsNumberCount")
+            totalCount[cardNumber + it + 1] = checkNotNull(totalCount[cardNumber + it + 1]) + checkNotNull(totalCount[cardNumber])
+
+        }
+    }
+
+    println(totalCount.map { it.value }.sum())
 }
