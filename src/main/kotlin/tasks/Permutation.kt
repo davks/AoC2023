ea6d03332896a3a7a48008693e82e299cc5426a9
davidk <davks11@gmail.com>
2024-08-13 23:41:57 +0200

    Permutations

---
+++ src/main/kotlin/tasks/Permutation.kt
@@ -0,0 +1,25 @@
+package tasks
+
+fun main() {
+    val items = listOf('a', 'b', 'c', 'd')
+    val permutations = permute(items)
+    permutations.forEach { println(it) }
+}
+
+fun <T> permute(list: List<T>): List<List<T>> {
+    if (list.isEmpty()) return listOf(emptyList())
+
+    val result = mutableListOf<List<T>>()
+
+    for (i in list.indices) {
+        val current = list[i]
+        println("current: $current")
+        val remaining = list.take(i) + list.drop(i + 1)
+        println("remaining: $remaining")
+        for (permutation in permute(remaining)) {
+            result.add(listOf(current) + permutation)
+        }
+    }
+
+    return result
+}
